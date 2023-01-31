1e2222c85e328735e6adbac83c712e424a866187
arikun11 <124012042+arikun11@users.noreply.github.com>
2023-01-31 10:41:13 +0700

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -9,7 +9,7 @@ export const $themeBreakpoints = {}
 // APP CONFIG
 export const $themeConfig = {
   app: {
-    appName: 'XC', // Will update name in navigation menu (Branding)
+    appName: '0xarikun', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
     appLogoImage: '/X.png', // Will update logo in navigation menu (Branding)
   },
